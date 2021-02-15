6f70b32e216c312b1fd4d0997ce28e20d4f4b63d
tomual <tomual@users.noreply.github.com>
2021-02-15 21:09:30 +0800

    Remove auth middleman for public portfolio pages

--- resources/views/portfolio/show.blade.php
+++ resources/views/portfolio/show.blade.php
@@ -7,14 +7,21 @@
 @section('content')
 <h1 class="page-title text-center w-100">{{ $post->title }}</h1>
 <hr class="divider">
-<div class="row">
-    <div class="col-lg-8 offset-lg-2">
-        <div>{!! nl2br($post->body) !!}</div>
+<div class="container">
+    
+    <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+            <div>{!! nl2br($post->body) !!}</div>
+        </div>
+        <div class="col-lg-12 text-center">
+            @foreach(json_decode($post->images) as $image)
+            <img src="{{ url('uploads') }}/{{ $image }}" class="mt-5 mx-auto">
+            @endforeach
+        </div>
+        <div class="col-12 text-center mt-5">
+            <a href="{{ route('works', ['tag' => $tag->id]) }}" class="btn btn-link d-inline-block m-auto p-3 back-to">« Back to {{ $tag->name }}</a>
+        </div>
     </div>
 </div>
-@foreach(json_decode($post->images) as $image)
-<img src="{{ url('uploads') }}/{{ $image }}" class="mt-5 mx-auto">
-@endforeach
-<a href="{{ route('works', ['tag' => $tag->id]) }}" class="btn btn-link text-light d-inline-block m-auto p-3">« Back to {{ $tag->name }}</a>
 
 @endsection
